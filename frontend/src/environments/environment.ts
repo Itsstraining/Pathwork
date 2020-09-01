a29752f3ddf199a4802c57087e996def43a9232e
vinh <fangireking789@gmail.com>
2020-09-01 12:17:47 +0700

    second

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  endpoint: "http://127.0.0.1:7762",
+  firebase:{
+    apiKey: "AIzaSyBYsO2FQUGwRjucjYOSXGG9zs7DXFbfQ_o",
+    authDomain: "trello-server-326e2.firebaseapp.com",
+    databaseURL: "https://trello-server-326e2.firebaseio.com",
+    projectId: "trello-server-326e2",
+    storageBucket: "trello-server-326e2.appspot.com",
+    messagingSenderId: "88335362408",
+    appId: "1:88335362408:web:b9990ca05a25de24de0edd",
+    measurementId: "G-SF09JZ5EBE"
+  }
 };
 
 /*

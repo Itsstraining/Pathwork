9031a3728ac9b22ddeb97be77b72ac04a87e683b
Trung Tin <tin.ct3229@sinhvien.hoasen.edu.vn>
2020-09-04 00:21:16 +0700

    change

--- backend/src/server.js
+++ backend/src/server.js
@@ -114,10 +114,10 @@ app.get("/v1/users", async (req, res) => {
   const user = req.body;
   console.log(user);
   try {
-      await admin.firestore().collection("store-user").add(user);
-      res.send("get user");
+    await admin.firestore().collection("store-user").add(user);
+    res.send("get user");
   } catch (e) {
-      res.send('cant get user');
+    res.send('cant get user');
   }
 })
 
@@ -125,41 +125,41 @@ app.post("/v1/users", async (req, res) => {
   const item = req.body;
   console.log(user);
   try {
-      await admin.firestore().collection("store-user").add(user);
-      res.send("creat user");
+    await admin.firestore().collection("store-user").add(user);
+    res.send("creat user");
   } catch (e) {
-      res.send('return user');
+    res.send('return user');
   }
 })
 
 app.put("/v1/users", async (req, res) => {
   const { user } = req.params;
   if (user == undefined) {
-      res.send({
-          message: "please set the user "
-      })
-      return;
+    res.send({
+      message: "please set the user "
+    })
+    return;
   }
   let doc = admin.firestore().collection("store-user").doc.apply(user);
   if ((await doc.get()).exists) {
-      if (id == req.body.user) {
-          try {
-              await doc.set(req.body);
-              res.send({
-                  message: "update successfully"
-              });
-              return;
-          }
-          catch{
-              res.send({
-                  message: "Fail"
-              });
-              return;
-          }
-          res.send({
-              message: "User is not match"
-          });
-          return;
+    if (id == req.body.user) {
+      try {
+        await doc.set(req.body);
+        res.send({
+          message: "update successfully"
+        });
+        return;
       }
+      catch{
+        res.send({
+          message: "Fail"
+        });
+        return;
+      }
+      res.send({
+        message: "User is not match"
+      });
+      return;
+    }
   }
 });

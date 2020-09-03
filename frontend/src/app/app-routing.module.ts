2353fb9fa4463ec6f7feb9a060fbd8a92d1a9194
dinhquoctuan <dinhquoctuan>
2020-09-04 00:03:34 +0700

    reponsive

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,32 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './pages/login/signin/signin/signin.component';
-import { SigoutComponent } from './pages/login/signout/sigout/sigout.component';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RegisterComponent } from './pages/login/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path : 'signin',
     component: SigninComponent,
+  },
+  {
+    path:'navbar',
+    component:NavbarComponent
+  },
+  {
+    path: 'sidebar',
+    component:SidebarComponent,
+  },
+  {
+    path: 'register',
+    component:RegisterComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
   },
 
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DashContentComponent } from './components/dash-content/dash-content/dash-content.component';
+import { DashBoxComponent } from './components/dash-box/dash-box/dash-box.component';
+import { DashItemComponent } from './components/dash-item/dash-item/dash-item.component';
+import { DashPageComponent } from './components/dash-page/dash-page/dash-page.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { UiModule } from './modules/ui/ui.module';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import { SigninComponent } from './pages/login/signin/signin/signin.component';
-import { SigoutComponent } from './pages/login/signout/sigout/sigout.component';
+
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -19,26 +22,35 @@ import {MatDialogModule} from '@angular/material/dialog';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
 
 import { MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
 import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
-import {DashBoxComponent} from '../app/components/dash-box/dash-box/dash-box.component';
-import {DashContentComponent} from './components/dash-content/dash-content/dash-content.component';
-import {DashItemComponent} from './components/dash-item/dash-item/dash-item.component';
-import {DashPageComponent} from './components/dash-page/dash-page/dash-page.component'
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterComponent } from './pages/login/register/register.component';
+
 @NgModule({
   declarations: [
     AppComponent,
 
     SigninComponent,
-    SigoutComponent,
-    DashBoxComponent,
+
     DashContentComponent,
+
+    DashBoxComponent,
     DashItemComponent,
     DashPageComponent,
+    NavbarComponent,
+    SidebarComponent,
+    HomeComponent,
+    RegisterComponent,
+
 
 
   ],
@@ -53,16 +65,20 @@ import {DashPageComponent} from './components/dash-page/dash-page/dash-page.comp
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireModule,
     AngularFireAuthModule,
+
     MatToolbarModule,
-    MatFormFieldModule,
-    MatInputModule,
     MatIconModule,
     MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
     MatDialogModule,
+
     MatDatepickerModule,
     MatNativeDateModule,
     MatSnackBarModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    NgbModule,
+    MatSelectModule,
   ],
 
   providers: [],

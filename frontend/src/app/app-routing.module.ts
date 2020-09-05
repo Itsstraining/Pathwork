b40bd6fcf33bb1fd87120a807cc4545b2b239e5b
vinh <fangireking789@gmail.com>
2020-09-05 13:22:28 +0700

    add board page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './pages/login/signin/signin/signin.component';
 import { SigoutComponent } from './pages/login/signout/sigout/sigout.component';
+import { BoardComponent } from './Components/board/board.component';
 
 const routes: Routes = [
   {
     path : 'signin',
     component: SigninComponent,
   },
-
+  {
+    path:"board",
+    component: BoardComponent,
+  }
 
 ];
 
